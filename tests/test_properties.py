a8eb641af3d431bae0200fb6249e7ec66597c5a0
Bill Allen <photo.allen@gmail.com>
2020-09-28 15:47:29 -0600

    Initial commit.

---
+++ tests/test_properties.py
@@ -0,0 +1,23 @@
+# vim: expandtab tabstop=4 shiftwidth=4
+
+import unittest
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import minnow
+
+class TestProperties(unittest.TestCase):
+    def test_save_load(self):
+        start_props = {'foo': 1, 'bar': 2, 'baz': 'boo', 'robert': 'shrubber'}
+
+        with TemporaryDirectory() as temp_dir:
+            prop_file_path = Path(temp_dir).joinpath('foo.properties')
+            minnow.save_properties(start_props, prop_file_path)
+            loaded_props = minnow.load_properties(prop_file_path)
+
+            for k, v in start_props.items():
+                self.assertTrue(k in loaded_props)
+
+                if k in loaded_props:
+                    self.assertEqual(str(v), loaded_props[k])
---
+++ tests/test_utils.py
@@ -0,0 +1,36 @@
+# vim: expandtab tabstop=4 shiftwidth=4
+
+import unittest
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import minnow
+
+class TestDataMetadataPair(unittest.TestCase):
+    def test_instantiation(self):
+        dmd = minnow.DataMetadataPair(Path('/foo'), Path('/foo.properties'))
+        print(dmd)
+
+    def test_list_pairs_at_path(self):
+        with TemporaryDirectory() as temp_dir:
+            temp_dir = Path(temp_dir)
+            base_names = ['foo', 'bar', 'baz']
+
+            for name in base_names:
+                temp_dir.joinpath(name).touch()
+                temp_dir.joinpath(name+'.properties').touch()
+
+            # throw in a couple unpaired files
+            temp_dir.joinpath('roger').touch()
+            temp_dir.joinpath('shrubber.properties').touch()
+
+            pairs = minnow.list_pairs_at_path(temp_dir)
+            self.assertEqual(len(pairs), 3)
+
+            found = {k:False for k in base_names}
+
+            for pair in pairs:
+                found[pair.data_path.name] = True
+
+            self.assertTrue(all(found.values()))
