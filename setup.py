74c8620677659388aa5cc6c1d6926e0cdb898dd8
Bill Allen <photo.allen@gmail.com>
2020-09-29 10:13:34 -0600

    Updated version number.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), 'r') as f:
 
 setup(
     name='minnow',
-    version='2020.272.0',
+    version='2020.273.0',
     author='Bill Allen',
     author_email='photo.allen@gmail.com',
     description='Utilities for the Minnow file processing framework.',
